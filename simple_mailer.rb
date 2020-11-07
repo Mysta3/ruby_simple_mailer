4b4929a076d9beb15f20a7580637a280a4570559
mysta3 <myshawnestallings@gmail.com>
2020-11-06 21:46:11 -0800

    added method in conditional

--- simple_mailer.rb
+++ simple_mailer.rb
@@ -3,6 +3,7 @@
 ## Have it run again
 ## currently used from within terminal
 ## maybe set a timer for message display to simulate message sent
+user_input = ''
 
 def message()
     #grab input from user through CLI
@@ -12,10 +13,8 @@ def message()
    from_name = gets.chomp
    puts "What is your message?" 
    message = gets.chomp
-
    #timestamp message
    time = Time.now
-
    #message output
    puts "---BEGIN OF MESSAGE ---" 
    puts "Message Received at: #{time.strftime("%I:%M %p")}"
@@ -27,23 +26,25 @@ def message()
    #message received notification
 end
 
-#Ask do you want to send another message?
-user_input = ''
+
 def userInput()
     puts "Would you like to Send a New Message?"
     puts "Enter answer format Y/N"
 end
+
+message()
+#Ask do you want to send another message?
 userInput()
 user_input = gets.chomp.downcase
 #Logic: if yes, display message in console, if no ask a question
 # question = "Would you like to send a message?"
-
 if user_input == 'y'
     message()
 elsif user_input == 'n'
     puts "Goodbye"
 else
     puts "Not a valid option"
+    message()
 end
 #open message method
 #prompt would you like to open message?
